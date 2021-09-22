2237a2d269da4b5bf33a0df1da0fceb5e0695ed4
Pyrdacor <trobt@web.de>
2021-09-22 13:24:18 +0200

    Added NPC Kasimir

---
+++ german/Amberfiles/pack.bat
@@ -0,0 +1,4 @@
+AmbermoonPack.exe AMNP Monster_char_data Monster_char_data.amb
+AmbermoonPack.exe AMNP NPC_char NPC_char.amb
+AmbermoonPack.exe AMNP 2Map_data 2Map_data.amb
+AmbermoonTextImport.exe -i . NPC_texts.amb NPC_texts
