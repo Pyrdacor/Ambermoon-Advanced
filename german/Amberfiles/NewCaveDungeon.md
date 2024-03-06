7c2aed9129d5ce68a9668d1bd09a6478b054da2e
Pyrdacor <trobt@web.de>
2024-03-06 15:45:04 +0100

    Up to date hopefully

---
+++ german/Amberfiles/NewCaveDungeon.md
@@ -0,0 +1,21 @@
+New labdata 41.
+New palette 51.
+
+## Walls
+
+-   101 (65): Normal cave wall
+-   102 (66): Normal cave wall with 1 plant right
+-   103 (67): Normal cave wall with 1 plant left
+-   104 (68): Normal cave wall with 2 plants
+-   105 (69): White wall **need to add**
+-   106 (6A): Exit **need to add**
+
+## Objects
+
+-   01 (01): Strange plant (texture 182)
+-   02 (02): Strange plant (texture 230)
+
+## Object infos
+
+-   01 (01): Strange plant (texture 182)
+-   02 (02): Strange plant (texture 230)
---
+++ german/Amberfiles/pack_comp.bat
@@ -0,0 +1,43 @@
+start "" AmbermoonPack.exe AMPC 1Map_data 1Map_data.amb -d1 -c3
+start "" AmbermoonPack.exe AMPC 2Map_data 2Map_data.amb -d4 -c3
+start "" AmbermoonPack.exe AMPC 3Map_data 3Map_data.amb -d4 -c3
+start "" AmbermoonPack.exe AMPC 2Lab_data 2Lab_data.amb -d4 -c3
+start "" AmbermoonPack.exe AMPC 2Lab_data 3Lab_data.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP 1Icon_gfx 1Icon_gfx.amb -d1 -c3
+start "" AmbermoonPack.exe AMNP 2Icon_gfx 2Icon_gfx.amb -d1 -c3
+start "" AmbermoonPack.exe AMNP 3Icon_gfx 3Icon_gfx.amb -d1 -c3
+start "" AmbermoonPack.exe AMNP 2Object3D 2Object3D.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP 3Object3D 3Object3D.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP 2Overlay3D 2Overlay3D.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP 3Overlay3D 3Overlay3D.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP 2Wall3D 2Wall3D.amb -c3
+start "" AmbermoonPack.exe AMNP 3Wall3D 3Wall3D.amb -c3
+
+start "" AmbermoonPack.exe LOB Combat_graphics_ext/001 Combat_graphics -c3
+start "" AmbermoonPack.exe JH+LOB Dict/001 Dict.amb 0xd2e7 -c3
+start "" AmbermoonPack.exe AMPC Icon_data Icon_data.amb -d1 -c3
+start "" AmbermoonPack.exe AMPC Monster_char Monster_char.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP Monster_gfx Monster_gfx.amb -d1 -c3
+start "" AmbermoonPack.exe AMNP Monster_groups Monster_groups.amb -d4 -c3
+start "" AmbermoonPack.exe AMPC NPC_char NPC_char.amb -d4 -c3
+start "" AmbermoonPack.exe AMNP NPC_gfx NPC_gfx.amb -d1 -c3
+start "" AmbermoonPack.exe LOB Object_icons_ext/001 Object_icons -c3
+start "" AmbermoonPack.exe JH+LOB Objects/001 Objects.amb 0xd2e7 -c3
+start "" AmbermoonPack.exe JH+LOB Place_data_dec/001 Place_data 0xd2e7 -c3
+start "" AmbermoonPack.exe AMPC Portraits Portraits.amb -d1 -c3
+start "" AmbermoonPack.exe AMNP Travel_gfx Travel_gfx.amb -c3
+start "" AmbermoonPack.exe AMNP Floors Floors.amb -d1 -c3
+start "" AmbermoonPack.exe AMPC Palettes Palettes.amb -d4
+
+start "" AmbermoonTextManager.exe -i . AllTexts -f 1Map_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f 2Map_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f 3Map_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f NPC_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f Object_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f Party_texts.amb -c
+start "" AmbermoonTextManager.exe -i . AllTexts -f Text.amb -c
+
+start "" AmbermoonPack.exe AMBR Save.00/Chest_data Save.00/Chest_data.amb
+start "" AmbermoonPack.exe AMBR Save.00/Party_char Save.00/Party_char.amb
+start "" AmbermoonPack.exe AMBR Save.00/Merchant_data Save.00/Merchant_data.amb
+start "" AmbermoonPack.exe AMBR Save.00/Automap Save.00/Automap.amb
